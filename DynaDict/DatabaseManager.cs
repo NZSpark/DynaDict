f51b46bde35243417ef71ea6280b7b9888a6dbcb
Spark Zheng <sparkzh@solutiondynamics.com>
2018-12-04 10:47:21 +1300

    add sqlite operations.

---
+++ DynaDict/DatabaseManager.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Database.Sqlite;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace DynaDict
+{
+    class DatabaseManager
+    {
+        private const string DatabaseFileName = "DynaDict.sqlite";
+        private const string DatabaseSource = "data source=" + DatabaseFileName + ";Version=3;";
+        SQLiteConnection m_dbConnection = new SQLiteConnection(DatabaseSource);
+        public void InitializeDB()
+        {
+            SQLiteConnection.CreateFile(DatabaseFileName);
+
+            m_dbConnection.Open();
+
+            string SQL_CREATE_DictList =
+               "CREATE TABLE DictList (" +
+               "DictID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
+               "DictName TEXT)";
+            SQLiteCommand command = new SQLiteCommand(SQL_CREATE_DictList, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            command.CommandText =
+                "CREATE TABLE URLList (" +
+                "ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                "DictID INTEGER," +
+                "URL TEXT)";
+            command.ExecuteNonQuery();
+
+            command.CommandText =
+                "CREATE TABLE Vocabulary (" +
+                "WordID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                "WordName TEXT," +
+                "Phonics TEXT," +
+                "ChineseDefinition TEXT," +
+                "EnglishDefinition TEXT)";
+            command.ExecuteNonQuery();
+
+
+            command.CommandText =
+                "CREATE TABLE DictWord (" +
+                "WordID INTEGER PRIMARY KEY," +
+                "DictID INTEGER PRIMARY KEY)";
+            command.ExecuteNonQuery();
+
+            m_dbConnection.Close();
+
+        }
+
+        public void StoreDictToDB(DictDataModel ddm)
+        {
+            string sDictID = "";
+
+            m_dbConnection.Open();
+
+            //insert DictList
+            SQLiteCommand command = new SQLiteCommand("insert into DictList (DictName) values ('" + ddm.sDictName + "')", m_dbConnection);
+            sDictID = command.ExecuteNonQuery().ToString();
+
+            //insert URLList
+            for (int i = 0; i < ddm.sSourceLinks.Length; i++)
+            {
+                command.CommandText = "insert into URLList (DictID, URL) values ('" + sDictID + "','" + ddm.sSourceLinks[i] + "')";
+                command.ExecuteNonQuery();
+            }
+
+            //insert Vocabulary and DictWord
+            for (int i = 0; i < ddm.DictWordList.Length; i++)
+            {
+
+                string sWordID = "";
+                string sE = "";
+                string sC = "";
+
+                for(int j = 0; j < ddm.DictWordList[i].sChineseDefinition.Length; j++)
+                {
+                    sC = sC + ddm.DictWordList[i].sChineseDefinition[j] + "\n";
+                }
+
+                for (int j = 0; j < ddm.DictWordList[i].sEnglishDefinition.Length; j++)
+                {
+                    sE = sE + ddm.DictWordList[i].sEnglishDefinition[j] + "\n";
+                }
+
+                command.CommandText = "insert into Vocabulary (WordName,Phonics,ChineseDefinition,EnglishDefinition) values ('" + ddm.DictWordList[i].sVocabulary + "','" + ddm.DictWordList[i].sPhonics + "','" + sC + "','" + sE + "')";
+                sWordID = command.ExecuteNonQuery().ToString();
+
+                command.CommandText = "insert into DictWord (WordID,DictID) values ('" + sWordID + "','" + sDictID + "')";
+                command.ExecuteNonQuery();
+            }
+
+            m_dbConnection.Close();
+        }
+
+        public DictDataModel GetDictFromDBByName(string DictName)
+        {
+            DictDataModel ddm = new DictDataModel();
+            
+            string sDictID = "";
+            m_dbConnection.Open();
+                        
+            SQLiteCommand command = new SQLiteCommand("select DictID from DictList where DictName = '" + DictName + "'" , m_dbConnection);
+
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    sDictID = reader["DictID"].ToString();
+                    Console.WriteLine(sDictID);
+                }
+            }
+
+            command.CommandText = "select URL from URLList where DictID = '" + sDictID + "'";
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    ddm.sSourceLinks.Append(reader["URL"].ToString());
+                    Console.WriteLine(reader["URL"].ToString());
+                }
+            }
+
+            command.CommandText = "select * from Vocabulary where WordID in (select WordID from DictWord where DictID = '" + sDictID + "' )";
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    VocabularyDataModel vdm = new VocabularyDataModel();
+                    vdm.sVocabulary = reader["WordName"].ToString();
+                    vdm.sPhonics = reader["Phonics"].ToString();
+                    vdm.sEnglishDefinition = reader["EnglishDefinition"].ToString().Split(new char [] { '\n' });
+                    vdm.sChineseDefinition = reader["ChineseDefinition"].ToString().Split(new char [] { '\n' });
+                    ddm.DictWordList.Append(vdm);                    
+                }
+            }
+
+            m_dbConnection.Close();
+            return ddm;
+        }
+
+        public string [] GetDictNameList()
+        {
+            string[] dictNameList = { };
+
+            m_dbConnection.Open();
+
+            SQLiteCommand command = new SQLiteCommand("select DictName from DictList", m_dbConnection);
+
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    dictNameList.Append(reader["DictName"].ToString());
+                }
+            }
+
+            m_dbConnection.Close();
+            return dictNameList;
+        }
+    }
+}
---
+++ DynaDict/DictDataModel.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace DynaDict
+{
+    class DictDataModel
+    {
+        public string sDictName = "embryo";
+        public string [] sSourceLinks = {
+            "https://www.theguardian.com/science/2018/nov/28/scientist-in-china-defends-human-embryo-gene-editing",
+            "https://www.theguardian.com/science/2018/nov/26/worlds-first-gene-edited-babies-created-in-china-claims-scientist"
+        };
+        public VocabularyDataModel[] DictWordList;
+
+        public string LoadWebPage(string sWebPageURL)
+        {
+            string sResult = null;
+            WebResponse response = null;
+            StreamReader reader = null;
+
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(sWebPageURL);
+                request.Method = "GET";
+                response = request.GetResponse();
+                reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+                sResult = reader.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                // handle error
+                System.Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                if (reader != null)
+                    reader.Close();
+                if (response != null)
+                    response.Close();
+            }
+            return sResult;
+        }
+
+        //Generate a list of unique words. 
+        public string[] GetWordListFromString(string sInput)
+        {
+            char [] sResult = sInput.ToArray();
+           
+            for (int i = 0; i < sResult.Length; i++)
+            {
+                //English word [A~Za~z];
+                if ((sResult[i] > 'A' && sResult[i] < 'Z') || (sResult[i] > 'a' && sResult[i] < 'z'))
+                    continue;
+                sResult[i] = ' ';
+            }
+
+            string [] sWordList = new string(sResult).Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            sWordList = sWordList.Distinct().ToArray();
+            return sWordList;
+        }
+
+
+
+    }
+
+    class VocabularyDataModel
+    {
+        public string sVocabulary = "embryo";
+        public string sPhonics = "ˈembrēˌō";
+
+        public bool bShowChineseDefinition = true;
+
+        public string [] sEnglishDefinition = {
+            "an unborn or unhatched offspring in the process of development." ,
+            "the part of a seed that develops into a plant, consisting (in the mature embryo of a higher plant) of a plumule, a radicle, and one or two cotyledons."
+        };
+        public string[] sChineseDefinition = {"胎" };
+        public string[] sSentences = { "They are engaging in an embryo research." };
+
+        public string GetDefinition(string sWord)
+        {
+            string sDefinition = "";
+
+            return sDefinition;
+        }
+
+        //http://www.dict.cn/embryo
+        public void ExtractDefinitionFromDicCN(string sInput)
+        {
+            string [] sED = { "" };
+            sEnglishDefinition = sED;
+            sChineseDefinition = sED;
+            sSentences = sED;
+        }
+
+    }
+}

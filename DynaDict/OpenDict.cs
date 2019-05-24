1d907ef96603f07993a9a54c3d00a9c226df8abc
spark-zheng <spark.zheng@solutiondynamics.com>
2019-05-24 23:39:21 +1200

    add swipe feature

--- DynaDict/OpenDict.cs
+++ DynaDict/OpenDict.cs
@@ -16,6 +16,9 @@ namespace DynaDict
 {
     public class OpenDict : Fragment
     {
+        private float _viewX;
+        private int _CurrentWordID = 0;
+
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -38,13 +41,68 @@ namespace DynaDict
             DatabaseManager dm = new DatabaseManager();
             DictDataModel ddm = dm.GetDictFromDBByName("SecurityMDS");
 
+            view.Touch += (s, e) =>
+            {
+                var handled = false;
+
+                switch (e.Event.Action)
+                {
+                    case MotionEventActions.Down:
+                        _viewX = e.Event.GetX();
+                        handled = true;
+                        break;
+                    case MotionEventActions.Up:
+                        var left = (int)(_viewX - e.Event.GetX() );
+                        if (left > 250)
+                        {
+                            _CurrentWordID++;
+                            if (_CurrentWordID == ddm.DictWordList.Count)
+                                _CurrentWordID = 0;
+
+                        }
+                        if (left < -250)
+                        {
+                            _CurrentWordID--;
+                            if (_CurrentWordID < 0)
+                                _CurrentWordID = ddm.DictWordList.Count - 1;
+                        }
+                        tvWordName.Text = ddm.DictWordList[_CurrentWordID].sVocabulary;
+                        tvPhonics.Text = ddm.DictWordList[_CurrentWordID].sPhonics;
+                        tvChineseDefinition.Text = string.Join(Environment.NewLine, ddm.DictWordList[_CurrentWordID].sChineseDefinition.ToArray());
+                        tvEnglishDefinition.Text = string.Join(Environment.NewLine, ddm.DictWordList[_CurrentWordID].sEnglishDefinition.ToArray());
+                        tvSentences.Text = string.Join(Environment.NewLine, ddm.DictWordList[_CurrentWordID].sSentences.ToArray());
+
+                        handled = true;
+                        break;
+                }
+
+
+
+                /*
+                if (e.Event.Action == MotionEventActions.Down)
+                {
+                    // do stuff
+                    handled = true;
+                }
+                else if (e.Event.Action == MotionEventActions.Up)
+                {
+                    // do other stuff
+                    handled = true;
+                }
+                */
+
+                e.Handled = handled;
+            };
+
+
             if (ddm.DictWordList.Count > 0)
             {
-                tvWordName.Text = ddm.DictWordList[0].sVocabulary;
-                tvPhonics.Text = ddm.DictWordList[0].sPhonics;
-                tvChineseDefinition.Text = string.Join(Environment.NewLine,ddm.DictWordList[0].sChineseDefinition.ToArray());
-                tvEnglishDefinition.Text = string.Join(Environment.NewLine, ddm.DictWordList[0].sEnglishDefinition.ToArray());
-                tvSentences.Text = string.Join(Environment.NewLine, ddm.DictWordList[0].sSentences.ToArray());
+                _CurrentWordID = 0;
+                tvWordName.Text = ddm.DictWordList[_CurrentWordID].sVocabulary;
+                tvPhonics.Text = ddm.DictWordList[_CurrentWordID].sPhonics;
+                tvChineseDefinition.Text = string.Join(Environment.NewLine, ddm.DictWordList[_CurrentWordID].sChineseDefinition.ToArray());
+                tvEnglishDefinition.Text = string.Join(Environment.NewLine, ddm.DictWordList[_CurrentWordID].sEnglishDefinition.ToArray());
+                tvSentences.Text = string.Join(Environment.NewLine, ddm.DictWordList[_CurrentWordID].sSentences.ToArray());
             }
 
             return view; 
